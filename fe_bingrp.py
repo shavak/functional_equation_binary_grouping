2f0c4cfc3219c2fbd7ed06f01c25c43ec052923a
Shavak Sinanan <shavak@gmail.com>
2023-08-20 16:22:17 +0100

    Added the problem statement and made the formula function more succint.

--- fe_bingrp.py
+++ fe_bingrp.py
@@ -1,27 +1,25 @@
+"""
+The function f is defined on the positive integers as follows:
+    f(1) = 1
+    f(2n) = f(n) if n is even
+    f(2n) = 2f(n) if n is odd
+    f(2n + 1) = 2f(n) + 1 if n is even
+    f(2n + 1) = f(n) if n is odd
+Find the number of positive integers n such that n < 2021 and f(n) = f(2021).
+"""
+
 def fe_table(n):
     f = {1 : 1}
     for k in range(2, n + 1):
         m = k // 2
         r = k % 2
         l = m % 2
-        if (r == 0):
-            if (l == 0):
-                f[k] = f[m]
-            else:
-                f[k] = 2 * f[m]
-        else:
-            if (l == 0):
-                f[k] = 2 * f[m] + 1
-            else:
-                f[k] = f[m]
+        f[k] = ((r ^ l) + 1) * f[m] + r * (1 - l)
     return f
 
 N = 2021
 
-ans = 0
+ans = -1
 f = fe_table(N)
 for j in f:
-    ans += (f[j] == f[2021])
-    
-ans -= 1
-                
+    ans += (f[j] == f[N])                
